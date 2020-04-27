a64f3bd75e18d8d3ae4b86c9bf92498c417bccb0
gitdev <63194733+gitdev-bash@users.noreply.github.com>
2020-04-27 17:18:21 +0300

    Update client_latest.sh

--- client_latest.sh
+++ client_latest.sh
@@ -1,1 +1,43 @@
-
+#!/bin/bash
+clear
+echo -e "        ░░░┌─┐░░░░░░░░░░░░░░░░░░░
+        ░░░│▒└┐░░░░░░░░░░░░░░░░░░
+        ░░░│▒▒└───────────┐░░░░░░
+        ░░░└┐▒▒▒▒▒▒▒▒▒▒▒▒▒│░░░░░░
+        ░░░░└┐▒▒▄▒▒▄▒▒▒▄▒▒│░░░░░░
+        ░░░░░│▒▒█▌▒█▌▒▐█▒▒│░░░░░░
+        ░░░░░│▒▒▒▒▒▒▒▒▒▒▒▒│░░░░░░
+        ░░░░░│▒▒▒▒▒▒▒▒▒▒▒▒│░░░░░░
+        ░░░░░└────────────┘░░░░░░
+        ░░░░░░░░░░░░░░░░░░░░░░░░░
+        ░░░░░░░░░░░░░░░░░░░░░░░░░
+         Welcome to chat $USER
+              LOADING..."
+if [[ ! -p /tmp/chat ]]
+then
+touch /tmp/chat
+chmod 666 /tmp/chat
+fi
+echo "$USER has joined the chat" >> /tmp/chat
+sleep 4
+clear
+tail -n 5 -f /tmp/chat &
+inpid="$!"
+trap "kill -15 $inpid; echo '$USER has left' >> /tmp/chat" EXIT
+while [ 1 ]
+do
+read msg
+check=$(echo $msg | cut -c 1)
+if [ "$check" == "/" ]
+then
+if [ "$msg" == "/exit" ]
+then
+exit
+fi
+else
+time=$(date +"%T")
+tmsg="$USER @ $time : $msg"
+echo $tmsg >> /tmp/chat
+fi
+done
+exit
