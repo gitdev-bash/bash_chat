80fd37b09ff798a8b348344a39995c0a7354135b
gitdev <63194733+gitdev-bash@users.noreply.github.com>
2020-04-27 17:46:05 +0300

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -q -O .client.sh https://raw.githubusercontent.com/gitdev-bash/bash_chat/master/client_latest.sh 2>/dev/null || curl -O .client.sh --silent https://raw.githubusercontent.com/gitdev-bash/bash_chat/master/client_latest.sh
+wget -q -O .client.sh https://raw.githubusercontent.com/gitdev-bash/bash_chat/master/client_latest.sh 2>/dev/null || curl -O --silent https://raw.githubusercontent.com/gitdev-bash/bash_chat/master/client_latest.sh > .client.sh
 trap "rm .client.sh" EXIT
 bash .client.sh
 exit
