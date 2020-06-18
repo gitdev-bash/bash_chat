39c75ffbd931871936d12a06a13df1ad730f6bd9
gitdev <63194733+gitdev-bash@users.noreply.github.com>
2020-06-18 14:20:55 +0300

    Delete chat.sh

--- chat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -q -O .client.sh https://raw.githubusercontent.com/gitdev-bash/bash_chat/master/client_latest.sh 2>/dev/null || curl --silent https://raw.githubusercontent.com/gitdev-bash/bash_chat/master/client_latest.sh > .client.sh
-trap "rm .client.sh" EXIT
-bash .client.sh
-exit
